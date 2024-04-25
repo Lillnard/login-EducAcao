efb1bf44e9c82c061e3c612c54899d72fa4acb17
Bruna_Finardi <b_finardi@hotmail.com>
2024-04-25 12:22:18 -0300

    ajuste form

--- cad-script.js
+++ cad-script.js
@@ -1,3 +1,25 @@
+const handlesubmit = (event) => {
+    event.preventDefault();
+
+    const nome = document.querySelector('input[name=nome]').value;
+    const sobrenome = document.querySelector('input[name=sobrenome]').value;
+    const cpf = document.querySelector('input[name=cpf]').value;
+    const disciplina = document.querySelector('input[name=disciplina]').value;
+    const email = document.querySelector('input[name=email]').value;
+
+    fetch('https://api.sheetmonkey.io/form/pjUppHgumYJpwseiAoBj97', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nome, sobrenome, cpf, disciplina, email }),
+    });
+}
+
+document.getElementById('cad-prof').addEventListener('submit', handlesubmit);
+
+
 // // Seleciona todos os elementos input dentro das labels
 // const inputs = document.querySelectorAll("label input");
 
